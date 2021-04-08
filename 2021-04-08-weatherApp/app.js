c9c8d46fbe4645461e18c1d419979f6a6049d343
DESKTOP-1P0J4O1\user <jkim68888@gmail.com>
2021-04-08 14:58:31 +0900

    날씨 앱 구현 완료

--- 2021-04-08-weatherApp/app.js
+++ 2021-04-08-weatherApp/app.js
@@ -25,10 +25,19 @@ function displayResults(weather){
     let now = new Date();
     let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>˚C</span>`;
+    
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+    
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}˚C / ${Math.round(weather.main.temp_max)}˚C`;
 }
 
 function dateBuilder(d){
-    let month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
     let day = days[d.getDay()];
